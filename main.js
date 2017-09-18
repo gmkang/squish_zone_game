541359033e49b392a36fa07280d79b75a7e9145f
Matthew Kim <matthewkim@Matthews-MacBook-Air.local>
2017-09-18 09:26:34 -0400

    game mods still in progress

--- main.js
+++ main.js
@@ -1,216 +1,241 @@
-var jq = $(function(){
-	console.log("JS OKAY!");
+// var jq = 
+$(function() {
+    console.log("JS OKAY!");
 
-var body = $('body');
-var timeLeft = 45;
-var score = 0;
-
-
-
-function createAnt() { 
- 	// var ant = document.createElement('div').
- 	// 	ant.setAttribute('class', 'ants');
-		// body.appendChild(ant);
-
-	var ant = $('<div class="ants"></div>');
-		body.append(ant);
-
-			ant.css("left", Math.random() * window.innerWidth);
-
-		setInterval(function() {
-			ant.css("left", Math.random() * window.innerWidth);
-			}, 1500)
-
-			// attach listener here!
-			ant.click(squishAnts);
-			// ant.on("click", function(){
-			// 	ant.css("left", "");
-			// })
-}
-createAnt();
+    var body = $('body');
+    var flies = $('.flies');
+    var ants = $('.ants');
+    var timeLeft = 45;
+    var score = 0;
 
 
+  $('#instructions').click(function(){
+  	$('#drop_down').slideToggle('slow'); 
+  });
 
-function createFly() {
-	var fly = $('<div class="flies"></div>');
-		body.append(fly);
 
-		fly.css("top", Math.random() * window.innerHeight);
-		fly.css("left", Math.random() * window.innerWidth);
+    function createAnt() {
 
-		setInterval(function() {
-			fly.css("top", Math.random() * window.innerHeight);
-			fly.css("left", Math.random() * window.innerWidth);
-		}, 300)
-			fly.on("click", function() {
-				fly.css("top", ""); 
-				fly.css("left", "");
-			})
+        var ant = $('<div class="ants"></div>');
+        body.append(ant);
 
-			fly.click(squishFlies);
+        ant.css("left", Math.random() * window.innerWidth);
 
-			// if(score === 60 || score === 65 || score === 160) {
-			// 	activateBugSpray();
-			// }
+        var intervalId = setInterval(function() {
+            ant.css("left", Math.random() * window.innerWidth);
+        }, 1500)
 
-	// var fly = document.createElement('div');
-	// fly.setAttribute('class', 'flies');
-		
-	// document.body.appendChild(fly);
+        // attach listener here!
+        ant.click({ intervalId: intervalId }, squishAnts);
 
-	// 	fly.style.top = Math.random() * window.innerHeight;
-	// 	fly.style.left = Math.random() * window.innerWidth;
+        if (score >= 100) {
+            ant.mouseover({ intervalId: intervalId }, squishAnts);
+        }
+        // 	bugSpray();
+        // 	activateBugSpray();
+        // }
 
-	// 		setInterval(function(){
-	// 		fly.style.top = Math.random() * window.innherHeight;
-	// 		fly.style.left = Math.random() * window.innerWidth;
-	// 	}, 400)	
-}
-createFly();
 
-	// for(var i = 0; i < 3; i++) {
-	// 	createAnt();
-	// 	createFly();
-	// }
-	
-var timerId = setInterval(startTimer, 1000);
-
-function startTimer() {
-  
-	var timer = document.getElementById('timer');
-
-		if(timeLeft === 0) {
-		  setTimeout(function(){
-		  		alert('Time\'s Up!');
-		  	},1000);
-		  clearTimeout(timerId);
-		} else {
-		  timer.innerHTML = `00:${timeLeft}`; 
-		  	if (timeLeft < 10) {
-		  		timer.innerHTML = `00:0${timeLeft}`; 
-		  	}
-		  timeLeft --;
-	}
-}
-startTimer();
+    }
+    createAnt();
 
 
 
-function squishFlies(event) {
-		// var x = event.clientX;
-		// var y = event.clientY;
+    function createFly() {
 
-		score+=10;
-		console.log(score);
-		scoreboard();
-		// console.log(this)
-		var squish = event.currentTarget.classList;
-		squish.add('splat');
-		// event.target.style.top = y + "px";
-		// event.target.style.left = x + "px";
-		// console.log(this)
-		
-		// console.log(x);
-		// console.log(y);
+        var fly = $('<div class="flies"></div>');
+        body.append(fly);
+
+        fly.css("top", Math.random() * window.innerHeight);
+        fly.css("left", Math.random() * window.innerWidth);
+
+        var intervalId = setInterval(function() {
+            fly.css("top", Math.random() * window.innerHeight);
+            fly.css("left", Math.random() * window.innerWidth);
+        }, 300)
 
 
-		setInterval(function(){
-			squish.remove('flies');
-		},1000)
-		
-}		
+        fly.click({ intervalId: intervalId }, squishFlies);
 
+        if (score >= 100) {
+            fly.mouseover({ intervalId: intervalId }, squishFlies);
+        }
 
+    }
+    createFly();
 
-function squishAnts(event) {
-	score+=5;
-	console.log(score);
-	scoreboard();
+    for (var i = 0; i < 4; i++) {
+        createAnt();
+        createFly();
+    }
 
-	var squish = event.currentTarget.classList;
-	squish.add('squished');
+    var timerId = setInterval(startTimer, 1000);
 
-	setInterval(function() {
-		squish.remove('ants');
-	},1000)
-	
-}
+    function startTimer() {
 
+        var timer = document.getElementById('timer');
 
-
-	setInterval(bugSwarm, 15000);
-
-function bugSwarm(){
-	
-	for (var i = 0; i < 5; i++) {
-		createAnt();
-		createFly();
-	}
-}
-	setTimeout(function(){
-		clearInterval(bugSwarm, 30000)
-	})
+        if (timeLeft === 0) {
+            setTimeout(function() {
+                alert('Time\'s Up! Better call a terminator!');
+            }, 1000);
+            clearTimeout(timerId);
+        } else {
+            timer.innerHTML = `00:${timeLeft}`;
+            if (timeLeft < 10) {
+                timer.innerHTML = `00:0${timeLeft}`;
+            }
+            timeLeft--;
+        }
+    }
+    startTimer();
 
 
 
+    function squishFlies(event) {
+
+        score += 10;
+        console.log(score);
+        scoreboard();
+        
+        var squish = event.currentTarget.classList;
+        squish.add('splat');
+        console.log('in squishFlies. event.data:', event.data);
+        clearInterval(event.data.intervalId);
+
+        setInterval(function() {
+            squish.remove('flies');
+            checkWinner();
+        }, 1000)
+
+    }
 
 
 
-function scoreboard(){
-	var scoreboard = document.getElementById('scoreboard');
-	scoreboard.innerHTML = `${score}pts.`
+    function squishAnts(event) {
 
-		// if(score === 60) {
-		// 	alert('ACTIVATE BUG SPRAY!')
-		// }
-}
-scoreboard();
+        score += 5;
+        console.log(score);
+        scoreboard();
 
+        var squish = event.currentTarget.classList;
+        squish.add('squished');
+        clearInterval(event.data.inervalId);
 
-function bugSpray() {
-	// 
-	console.log(score)
-	
-	if (score === 60 || score === 160) {
-		var prize = $('<img src="prize1.png" id="bugspray">')
-		body.append(prize);
-			prize.show("slow");
-			prize.hide('fast');
-	}
-}
+        setInterval(function() {
+            squish.remove('ants');
+            checkWinner();
+        }, 1000)
 
-	function activateBugSpray(){
-	
-	// var ants = document.querySelectorAll('.ants');
-	// var flies = document.querySelectorAll('.flies');
-
-	if (event.keyCode === 32) {
-		document.removeEventListener("click", squishFlies);
-		document.removeEventListener("click", squishAnts);
-		
-
-		document.addEventListener("mouseover", squishFlies)
-		// 	var killFly = event.currentTarget.classList;
-		// 		killFly.add('splat')
-		// });
+    }
 
 
-		document.addEventListener("mouseover", squishAnts)
-		// 	var killAnt = event.currentTarget.classList;
-		// 		killAnt.add('squished')
-		// });
 
+    setInterval(bugSwarm, 25000);
+
+    function bugSwarm() {
+
+        for (var i = 0; i < 12; i++) {
+            createAnt();
+            createFly();
+        }
+    }
+
+
+    function stopBugSwarm() {
+        clearInterval(createAnt);
+        clearInterval(createFly);
+    }
+    setInterval(stopBugSwarm, 30000)
+
+
+
+    function scoreboard() {
+
+        var scoreboard = document.getElementById('scoreboard');
+        scoreboard.innerHTML = `${score}pts.`
+        if (score >= 100) {
+            bugSpray();
+        }
+    }
+    scoreboard();
+
+
+    function removeBugSpray() {
+        clearInterval(activateBugSpray);
+    }
+
+
+    function bugSpray() {
+
+        if (score >= 100) {
+            var prize = $('<img src="images/prize1.png" id="bugspray">')
+            body.append(prize);
+            prize.show();
+        }
+    }
+    bugSpray();
+
+
+
+    function activateBugSpray() {
+
+
+        if (event.keyCode === 32) {
+
+
+
+            $('.flies').on("mouseleave", squishFlies);
+            setInterval(function(){
+            	remove(flies)
+            }, 1000)
+
+            $('.ants').on("mouseover", squishAnts);
+            setInterval(function(){
+            	remove(ants)
+            }, 1000)
+
+            $('.flies').off("click", squishFlies);
+            $('.ants').off("click", squishAnts);
+
+
+        }
+
+        document.onmousemove = animateSpray;
+
+    }
+
+    document.onkeypress = activateBugSpray;
+
+
+    function animateSpray(event) {
+        var spray = document.createElement('div');
+        spray.setAttribute('class', 'spray');
+        document.body.appendChild(spray);
+
+        spray.style.left = event.clientX + "px"
+        spray.style.top = event.clientY + "px"
+
+        spray.style.transition = "all 0.5s linear 0s"
+
+        spray.style.width = "20px";
+        spray.style.height = "20px";
+        spray.style.left = spray.offsetLeft - 20 + 'px';
+        spray.style.top = spray.offsetTop - 20 + 'px';
+        spray.style.borderWidth = "5px";
+        spray.style.opacity = 0;
+    }
+
+
+    function checkWinner() {
+
+    		if(timeLeft > 0) {
+    			if($('.flies').length === 0 && $('.ants').length === 0) {
+    				alert('YAY! Your home is bug free!');
+    			} 
+    		}
 		}
-		
-		//change EVENT LISTENER PROPERTIES! MOUSEOVER CURSOR TOO
-	}
-
-	document.onkeypress = activateBugSpray;
-
-
-
-
-
+clearInterval(checkWinner);
 
 
 
